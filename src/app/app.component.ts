48c325dbeeed752923f7893318379bbc0938ce75
Or Shalmayev <shalmayev1993@gmail.com>
2021-02-16 20:29:10 +0200

    finished episode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  template: `
+    <div>
+      {{ title + '!' }}
+      <p>
+      {{ isHappy ? ':)' : ':(' }}
+      </p>
+      <button (click)="toggleHappy()">Toggle Happy</button>
+    </div>
+  `,
 })
 export class AppComponent {
-  title = 'ultimatecourse-basic';
+  public title: string;
+  public isHappy: boolean = true;
+
+  constructor(){
+    this.title = 'ultimatecourse-basic';
+  }
+
+  public toggleHappy() {
+    this.isHappy = !this.isHappy;
+  }
 }
