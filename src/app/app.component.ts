df526e97ce9d5e03c4cc691562508e27a3179abe
Or Shalmayev <shalmayev1993@gmail.com>
2021-02-20 22:50:13 +0200

    finished lesson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,6 +20,11 @@ import { Passenger } from './models/passenger';
             check in date:
             {{ passenger.checkedInDate ? (passenger.checkedInDate | date: 'yMMM' | uppercase) : 'not checked in'}}
           </div>
+          <!-- children -->
+          <div class="children">
+            <!-- ? safe navigation operator (if  this exists then continue to the next step) -->
+            children: {{ passenger.children?.length || 0 }}
+          </div>
         </li>
       </ul>
       <!-- <app-classandstyle [passengers]="passengers"></app-classandstyle> -->
@@ -32,31 +37,36 @@ export class AppComponent {
       id: 1,
       fullname: 'Or',
       checkedIn: true,
-      checkedInDate: 1233211233214
+      checkedInDate: 1233211233214,
+      children: [{name: 'Ted', age: 1}]
     },
     {
       id: 2,
       fullname: 'Or2',
       checkedIn: true,
-      checkedInDate: 1233211283214
+      checkedInDate: 1233211283214,
+      children: [{name: 'Ted2', age: 2}]
     },
     {
       id: 3,
       fullname: 'Or3',
       checkedIn: false,
-      checkedInDate: null
+      checkedInDate: null,
+      children: [{name: 'Ted3', age: 3}]
     },
     {
       id: 4,
       fullname: 'Or4',
       checkedIn: true,
-      checkedInDate: null
+      checkedInDate: null,
+      children: null
     },
     {
       id: 5,
       fullname: 'Or5',
       checkedIn: false,
-      checkedInDate: 1233211283184
+      checkedInDate: 1233211283184,
+      children: [{name: 'Ted5', age: 5}]
     },
   ];
   constructor(){ }
