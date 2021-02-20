2a6af90df7767a4b020347dd2b8ed873835193d9
Or Shalmayev <shalmayev1993@gmail.com>
2021-02-20 23:03:16 +0200

    finished lesson

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClassandstyleComponent } from './components/classandstyle/classandstyle.component';
 
+import { PassengerDashboardModule } from './passenger-dashboard/passeger-dashboard.module';
+
 @NgModule({
   declarations: [
     AppComponent,
     ClassandstyleComponent,
   ],
   imports: [
+    // Angular Modules
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // Custom Modules
+    PassengerDashboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
