1f78424b4691808f074bc40a532c0cd6aaf0d83a
Or Shalmayev <shalmayev1993@gmail.com>
2021-02-20 19:13:47 +0200

    completed lesson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
 import { Component } from '@angular/core';
+import { Passenger } from './models/passenger';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  template: `
+    <div class="app">
+      <h1>{{title}}</h1>
+      <!-- the template way -->
+      <ul>
+        <ng-template [ngForOf]="passengers" ngFor let-passenger let-i="index" >
+          <li>
+            {{ i }}:
+            {{passenger.fullname}}
+          </li>
+        </ng-template>
+      </ul>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i = index">
+          {{ i }}:
+          {{passenger.fullname}}
+        </li>
+      </ul>
+    </div>
+  `
 })
 export class AppComponent {
-  title = 'ultimatecourse-basic';
+  public title: string = 'Airline Passengers';
+  public passengers: Passenger[] = [
+    {
+      id: 1,
+      fullname: 'Or',
+      checkedIn: true
+    },
+    {
+      id: 2,
+      fullname: 'Or2',
+      checkedIn: true
+    },
+    {
+      id: 3,
+      fullname: 'Or3',
+      checkedIn: false
+    },
+    {
+      id: 4,
+      fullname: 'Or4',
+      checkedIn: true
+    },
+    {
+      id: 5,
+      fullname: 'Or5',
+      checkedIn: false
+    },
+  ];
+  constructor(){ }
 }
